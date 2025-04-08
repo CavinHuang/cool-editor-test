a98319f097c58a1c78cbf4ebb2812b4531d846d0
cavinHuang <943866180@qq.com>
2025-04-08 18:25:34 +0800

    添加基础项目结构，包括 ESLint 配置、.gitignore 文件、许可证、package.json、Rollup 配置及基本编辑器实现。新增多个插件和解析器以支持文本格式化、历史记录、文件处理等功能，同时引入 Prism.js 进行代码高亮显示。添加测试文件和示例以验证功能。

---
+++ rollup.config.js
@@ -0,0 +1,35 @@
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import postcss from 'rollup-plugin-postcss';
+import cleanup from 'rollup-plugin-cleanup';
+import { terser } from 'rollup-plugin-terser';
+export default {
+  input: './src/index.js',
+  output: {
+    dir: 'build',
+    format: 'es',
+    sourcemap: false,
+  },
+  plugins: [
+    resolve(),
+    postcss({
+      modules: true,
+      extract: true,
+    }),
+    babel({
+      babelrc: false,
+      plugins: [
+        [
+          '@babel/plugin-transform-react-jsx',
+          {
+            useBuiltIns: true,
+          },
+        ],
+      ],
+    }),
+    commonjs(),
+    // terser(),
+    // cleanup(),
+  ],
+};
